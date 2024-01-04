40566ff4cb18f52aeffb2681ab241d7027003e1d
neet-1405 <neeteshparashar66@gmail.com>
2024-01-04 10:30:40 +0530

    first commit

---
+++ backendyoutube/index.js
@@ -0,0 +1,48 @@
+// const express=require("express");
+// const dotenv=require("dotenv").config();
+// const cors=require("cors")
+// const mongoose=require("mongoose");
+// const bodyParser=require("body-parser");
+// const path = require("path");
+// const users=require("../backendyoutube/src/users")
+// const app=express();
+
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use(users);
+
+// app.listen(port,()=>{
+
+//     console.log(`server start at port no${Port}`)
+// })
+
+const express = require("express"); // Importing Express module
+// const app = require("./src/app"); // Importing custom app module
+
+const users=require("../backendyoutube/src/users")
+const mongoose = require("mongoose"); // Importing Mongoose module
+const port = process.env.port || 8020; // Setting the port number
+
+// Middleware to parse JSON bodies and URL-encoded bodies
+users.use(express.json()); // Parsing JSON bodies
+users.use(express.urlencoded({ extended: false })); // Parsing URL-encoded bodies
+
+// Connecting to the database
+// const DATABASE_URL =
+// "mongodb+srv://ojhaparth:test123@cluster0.9xedsar.mongodb.net/";
+
+
+MONGOOSE_URI= "mongodb://127.0.0.1:27017/subscribers";
+mongoose.connect(MONGOOSE_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err)); // Handling database connection errors
+db.once("open", () => console.log("connected to database")); // Logging successful database connection
+
+// Starting the server
+users.listen(port, () => console.log(`App listening on port ${port}!`)); 
---
+++ backendyoutube/src/users.js
@@ -0,0 +1,128 @@
+
+const express = require("express");
+const routes = express.Router();    
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require('mongoose');
+const Subscriber = require("./model/schema");
+const { error } = require("console");
+const app=express();
+
+
+
+
+// any platform can access my api
+// what evere data is coming in the body can not be redable body parser aloow it to be redable
+app.use(bodyParser.urlencoded({ extended:true}))
+app.use(bodyParser.json());
+
+
+
+
+
+// routes.use((req,ees,next)=>{
+//     if(mongoose.connection.readystate!==1){
+//         return res.status(500).send("Internal server error mongodb not connected")
+//     }
+//     next();
+// });
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../index.html"))
+});
+
+
+// Middleware to check MongoDB connection status
+routes.use((req, res, next) => {
+    if (mongoose.connection.readyState !== 1) {
+        return res.status(500).send("Internal server error: MongoDB not connected");
+    }
+    next();
+});
+
+// routes.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"/index.html"))
+// });
+
+
+app.get("/subscribers", async (req, res) => {
+    try {
+        let subscribers = await Subscriber.find({});
+        console.log(subscribers);
+        res.send(JSON.stringify({ subscribers}));
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal server error");
+    }
+});
+
+
+
+
+
+
+
+app.use(express.urlencoded({ extended:true}))
+app.use(express.json());
+
+
+
+
+
+
+
+//  find by id 
+app.get("/subscribers/:id",async(req,res)=>{
+
+    try{
+        const id=req.params.id;
+        if(!id){
+            res.statusMessage(400).json({message:"no id provided"});
+            return;
+        }
+        const Subscriberss=await Subscriber.findById(id);
+        if(!Subscriber){
+            res.status(400).json({message:"subscriber not found"})
+            return;
+        }
+        res.send(Subscriberss);
+    }
+    catch(error){
+        res.status(400).json({message: error.message});
+    }
+});
+
+
+// //THIS ROUTE PROVIDES AN ARRAY OF ALL SUBSCRIBERS WITH ONLY TWO FIELDS, THEIR NAME AND SUBSCRIBED CHANNEL.
+
+app.get("/subscriberss/names",async (req,res)=>{
+    
+    try{
+      
+
+
+        let Subscribersss =  await Subscriber.find(
+            {},
+            {name:1,subscribedChannel:1,_id:0});
+
+        res.status(200).json(Subscribersss);
+        // res.send(JSON.stringify({ Subscribersss}));
+        // res.send(subscribers)
+    }
+    catch(err){
+        res.status(400);
+       
+    }
+});
+
+
+
+// Dummy API route
+app.get('/dummyapi', (req, res) => {
+    res.send("Dummy API is working");
+});
+
+
+
+module.exports = app;
